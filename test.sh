8c8cc04243ac6390fb7bd03828e88b49efc8ce41
Rohhan Varma <rvarm1@fb.com>
2021-05-21 23:58:42 +0000

    foo

--- test.sh
+++ test.sh
@@ -9,4 +9,4 @@ echo "running python script"
 echo "world size "$SLURM_NTASKS
 echo "node name "$SLURMD_NODENAME
 echo "rank? "$SLURM_PROCID
-python dlrm_s_pytorch.py --use-gpu --print-time --dist-backend nccl --nepochs 2 --data-size 256 --mini-batch-size 2 --debug-mode --arch-sparse-feature-size 2 --node-world-size ${SLURM_NTASKS} --rank ${SLURM_PROCID}
+python dlrm_s_pytorch.py --use-gpu --print-time --dist-backend nccl --nepochs 2 --data-size 2048 --mini-batch-size 16 --debug-mode --arch-sparse-feature-size 2 --node-world-size ${SLURM_NTASKS} --rank ${SLURM_PROCID}
---
+++ test_large.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export MASTER_PORT=29501
+export MASTER_ADDR=$(scontrol show hostname ${SLURM_NODELIST} | head -n 1)
+
+echo "using master addr"$MASTER_ADDR
+echo "using part "$SLURM_PARTITION
+# TODO - minibatch size must be at least the no of local gpu?
+echo "running python script"
+echo "world size "$SLURM_NTASKS
+echo "node name "$SLURMD_NODENAME
+echo "rank? "$SLURM_PROCID
+python dlrm_s_pytorch.py --use-gpu --print-time --dist-backend nccl --nepochs 2 --data-size 2048 --mini-batch-size 16 --debug-mode --arch-sparse-feature-size 64 --arch-embedding-size 1000000-1000000-1000000-1000000-1000000-1000000-1000000-1000000-1000000-1000000-1000000-1000000 --arch-mlp-bot 2000-1024-1024-512-256-64 --arch-mlp-top 4096-4096-1 --node-world-size ${SLURM_NTASKS} --rank ${SLURM_PROCID}
